2d69edb209da7291085b89f98a962d6eb36ba013
Zachari Barnes <ZachariBarnes@Yahoo.com>
2019-10-24 16:01:26 -0500

    Changed lots

--- ui/src/components/InputForms.js
+++ ui/src/components/InputForms.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import getRolesTable from '../Functions/GlobalFunctions';
+import { Helmet } from 'react-helmet';
 import { Button, Form, Row, Col } from 'react-bootstrap';
 import { ResponseTable } from './ResponseTable'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
+var cloneDeep = require('lodash.clonedeep');
+const UNSUPPORTED_PLAYER_COUNT = 'The currently entered number of Players/Roles is currently unsupported.\nThere must be at least as many players as there are roles.\nReach out to ZachariBarnes@yahoo.com if you wish this to be a supported feature.'
 
 class InputForms extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             players: [],
+            // players: [
+            //     "Bob",
+            //     "george",
+            //     "fred",
+            //     "mercury",
+            //     "sara",
+            //     "zach",
+            //     "lauren",
+            //     "jimbo",
+            //     "mahdi",
+            //     "sam"],
             roles: ['Doctor', 'Seer', 'Mason', 'Mason', 'Hunter'],
             calcBgs: true,
-            bgRatio: 0.2,
+            bgRatio: 0.25,
             rolesGenerated: false,
+            assignments: [],
             postBody: undefined,
+            sortOrder: {
+                col: 'role',
+                order: "default"
+            },
+            error: false
         }
 
         this.handlePlayersChange = this.handlePlayersChange.bind(this);
         this.handleRolesChange = this.handleRolesChange.bind(this);
+        this.sortByRole = this.sortByRole.bind(this);
+        this.sortByPlayer = this.sortByPlayer.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.sortAssignmentsByProvidedRole = this.sortAssignmentsByProvidedRole.bind(this);
     }
 
     handlePlayersChange(event) {
@@ -30,47 +54,149 @@ class InputForms extends React.Component {
                 return "Laurne Free-Wifi-at-the Hilton";
             return e.trim()
         })
+        const error = this.state.error
+            && this.state.players.length <= this.state.roles.length
+            ? UNSUPPORTED_PLAYER_COUNT
+            : false;
         this.setState({
             ...this.state,
-            players
+            players,
+            error
         });
     }
+
     handleRolesChange(event) {
         const { value } = event.target
         let roles = value.indexOf(',') > 0 ? value.split(',') : value.split('\n');
         roles = roles.map(e => {
             return e.trim()
         })
+        const error = this.state.error
+            && this.state.players.length <= this.state.roles.length
+            ? UNSUPPORTED_PLAYER_COUNT
+            : false;
         this.setState({
             ...this.state,
-            roles
+            roles,
+            error
         });
+    }
+
+    sortByRole() {
+        let { assignments, sortOrder } = this.state;
+        const { col, order } = sortOrder;
+        const myOrder = col === 'player' || (order === 'default' || order === 'desc') ? 1 : -1;
+        assignments.sort((a, b) => {
+            if (a.Role < b.Role) {
+                return -1 * myOrder;
+            }
+            if (a.Role > b.Role) {
+                return 1 * myOrder;
+            }
+            // names must be equal
+            return 0;
+        });
+        this.setState({
+            ...this.state,
+            assignments,
+            sortOrder: {
+                col: 'role',
+                order: myOrder > 0 ? 'asc' : 'desc'
+            },
+            error: false
+        });
+    }
+
+    sortByPlayer() {
+        let { assignments, sortOrder } = this.state;
+        const { col, order } = sortOrder;
+        const myOrder = col === 'role' || (order === 'default' || order === 'desc') ? 1 : -1;
+        assignments.sort((a, b) => {
+            if (a.Player < b.Player) {
+                return -1 * myOrder;
+            }
+            if (a.Player > b.Player) {
+                return 1 * myOrder;
+            }
+            // names must be equal
+            return 0;
+        });
+        this.setState({
+            ...this.state,
+            assignments,
+            sortOrder: {
+                col: 'player',
+                order: myOrder > 0 ? 'asc' : 'desc'
+            }
+        })
     }
 
     handleSubmit() {
         const { players, roles, calcBgs, bgRatio } = this.state;
-        const postBody = {
-            players,
-            roles,
-            calcBgs,
-            bgRatio
+        if (players.length && roles.length && roles.length <= players.length) {
+            let assignments;
+            const postBody = {
+                players,
+                roles,
+                calcBgs,
+                bgRatio
+            }
+            assignments = getRolesTable(cloneDeep(postBody));
+            assignments = this.sortAssignmentsByProvidedRole(assignments)
+            this.setState({
+                ...this.state,
+                postBody,
+                rolesGenerated: true,
+                assignments
+            });
         }
-        this.setState({
-            ...this.state,
-            rolesGenerated: true,
-            postBody
+        else {
+            this.setState({
+                error: UNSUPPORTED_PLAYER_COUNT
+            });
+            console.log(`ERROR: ${UNSUPPORTED_PLAYER_COUNT}`);
+        }
+    }
+
+    sortAssignmentsByProvidedRole(assignments) {
+        const { roles } = this.state;
+        let sortedAssignments = [];
+        roles.forEach(role => {
+            let foundAssignment = assignments.find(a =>
+                a.Role === role);
+            sortedAssignments.push(foundAssignment)
+            assignments.splice(
+                assignments.indexOf(
+                    sortedAssignments[sortedAssignments.length - 1]), 1);
         });
+        assignments.sort((a, b) => {
+            if (a.Role < b.Role) {
+                return -1;
+            }
+            if (a.Role > b.Role) {
+                return 1;
+            }
+            // names must be equal
+            return 0;
+        });
+        sortedAssignments.push(...assignments)
+        return sortedAssignments;
     }
 
     render() {
         const { players } = this.state;
+        const TITLE = 'Role Randomizer';
         const DEFAULT_PLAYERS = 'Enter All Players Here';
         const DEFUALT_ROLES_DESCRIPTION = 'Enter Roles Here, OR\nLeave blank to use:\nDoctor,\nSeer,\nMason,\nMason,\nHunter';
         // const DEFAULT_ROLES = ['Doctor', 'Seer', 'Mason', 'Mason', 'Hunter'];
         // console.log(roles, DEFAULT_ROLES, roles == DEFAULT_ROLES)
         return (
-            <div>
+            <div className="widthController">
+                <Helmet>
+                    <title>{TITLE}</title>
+                </Helmet>
                 <Form>
+                    {!!this.state.error && <Row><Col><div className="error">{UNSUPPORTED_PLAYER_COUNT}</div></Col></Row>}
                     <Row>
                         <Col></Col>
                         <Col>
@@ -80,10 +206,14 @@ class InputForms extends React.Component {
                                     label="Players"
                                     multiline="15"
                                     rows="15"
-                                    value={players.length ? players : DEFAULT_PLAYERS}
+                                    defaultValue={players.length ? players : DEFAULT_PLAYERS}
                                     onChange={e => { this.handlePlayersChange(e) }}
                                     onClick={(e) => { e.currentTarget.select() }}
+                                    isInvalid={this.state.error}
                                 />
+                                <Form.Control.Feedback type="invalid">
+                                    Player or role Count Invalid
+                                </Form.Control.Feedback>
                             </Form.Group>
                         </Col>
                         <Col>
@@ -97,6 +227,7 @@ class InputForms extends React.Component {
                                     // value={roles == DEFAULT_ROLES ? DEFUALT_ROLES_DESCRIPTION : roles}
                                     onChange={e => { this.handleRolesChange(e) }}
                                     onClick={(e) => { e.currentTarget.select() }}
+                                    isInvalid={this.state.error}
                                 />
                             </Form.Group>
                         </Col>
@@ -160,7 +291,7 @@ class InputForms extends React.Component {
                     </Row>
                 </Form>
 
-                <ResponseTable body={this.state.postBody} />
+                <ResponseTable assignments={this.state.assignments} sortByRole={this.sortByRole} sortByPlayer={this.sortByPlayer} />
             </div >
         );
     };
