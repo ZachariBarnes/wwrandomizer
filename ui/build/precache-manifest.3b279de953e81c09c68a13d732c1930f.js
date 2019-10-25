50d1173a3b4fd97093d5d83aa50383647e2f4aa0
Zachari Barnes <ZachariBarnes@Yahoo.com>
2019-10-25 08:36:53 -0500

    pushing build Folder to see if It is viewable from repo

---
+++ ui/build/precache-manifest.3b279de953e81c09c68a13d732c1930f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971e37b6b1bcedb2533d5e449cdac209",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e510190346f1a744870d",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e12f7e831f8606b620e2",
+    "url": "./static/css/main.312993c7.chunk.css"
+  },
+  {
+    "revision": "e510190346f1a744870d",
+    "url": "./static/js/2.8a41e8a5.chunk.js"
+  },
+  {
+    "revision": "e12f7e831f8606b620e2",
+    "url": "./static/js/main.e6e3aded.chunk.js"
+  },
+  {
+    "revision": "a69221b436f7f1c2f088",
+    "url": "./static/js/runtime-main.544b61dc.js"
+  }
+]);
